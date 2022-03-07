1400ab17b756f08fb4a480854a4704727b01cd58
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-03-07 06:25:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Given two numbers m and n as input, find out their HCF. Use recursion. Do not use loops. Do a dry run for each test case.
+Hint : If m>=n, HCF(m,n) = n if m%n=0, otherwise it is HCF(n,m%n)
+Input-> m=100,n=64
+Output-> 4
+"""
